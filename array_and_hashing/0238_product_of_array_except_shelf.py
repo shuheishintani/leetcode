1980b0bb5dffe7ebe5a71c944bd1731aa26b44a5
Shuhei Shintani <shuheishintani.dev@gmail.com>
2023-01-08 22:01:36 +0900

    updated

---
+++ array_and_hashing/0238_product_of_array_except_shelf.py
@@ -0,0 +1,21 @@
+from typing import List
+
+
+class Solution:
+    # O(1) space approach
+    # - time: O(N)
+    # - space: O(1)
+    def productExceptSelf(self, nums: List[int]) -> List[int]:
+        res = [1] * (len(nums))
+
+        prefix = 1
+        for i in range(len(nums)):
+            res[i] = prefix
+            prefix *= nums[i]
+
+        postfix = 1
+        for i in range(len(nums) - 1, -1, -1):
+            res[i] *= postfix
+            postfix *= nums[i]
+
+        return res
