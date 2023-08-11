4463b6107781ee87398a5ff01f0ac95b78299c39
Shuhei Shintani <shuheishintani.dev@gmail.com>
2023-08-11 19:03:23 +0900

    updated

---
+++ greedy/2811_check_if_it_is_possible_to_split_array.py
@@ -0,0 +1,16 @@
+from typing import List
+
+
+class Solution:
+    def canSplitArray(self, nums: List[int], m: int) -> bool:
+        if len(nums) <= 2:
+            return True
+
+        for i in range(1, len(nums)):
+            if nums[i - 1] + nums[i] >= m:
+                return True
+        return False
+
+
+sol = Solution()
+print(sol.canSplitArray([1], 1))
